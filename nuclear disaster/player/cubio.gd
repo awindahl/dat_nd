01760c1603a2dcf930f361bde3a0616587fdcb0e
awindahl <alewin1994@gmail.com>
2021-05-09 00:52:16 +0200

    added movement and camera controls

--- nuclear disaster/player/cubio.gd
+++ nuclear disaster/player/cubio.gd
@@ -8,54 +8,58 @@ const DECELERATION = 4
 onready var camera = $Target/Camera
 onready var gravity = -ProjectSettings.get_setting("physics/3d/default_gravity")
 onready var start_position = translation
-var velocity: Vector3
+var yaw = 0
+var mouse_sensitivity = 1
+var my_rotation
+var target = null
+var velocity = Vector3.ZERO
+var speed = 2
 
+func _ready():
+	camera.get_parent().set_as_toplevel(true)
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		yaw = fmod(yaw - event.relative.x * mouse_sensitivity, 360)
+		my_rotation = event.relative
+		
+#	if event is InputEventMouseButton and event.button_index == 1 and event.pressed:
+#		var ray_length = 100000
+#		var from = camera.project_ray_origin(get_viewport().get_mouse_position())
+#		var to = from + camera.project_ray_normal(get_viewport().get_mouse_position()) * ray_length
+#
+#		var space_state = get_world().direct_space_state
+#		target = space_state.intersect_ray(from, to).position
+		
+		
 func _physics_process(delta):
-	if Input.is_action_just_pressed("exit"):
-		get_tree().quit()
-	if Input.is_action_just_pressed("reset_position"):
-		translation = start_position
-
-	var dir = Vector3()
-	dir.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	dir.z = Input.get_action_strength("move_back") - Input.get_action_strength("move_forward")
-
-	# Get the camera's transform basis, but remove the X rotation such
-	# that the Y axis is up and Z is horizontal.
-	var cam_basis = camera.global_transform.basis
-	var basis = cam_basis.rotated(cam_basis.x, -cam_basis.get_euler().x)
-	dir = basis.xform(dir)
-
-	# Limit the input to a length of 1. length_squared is faster to check.
-	if dir.length_squared() > 1:
-		dir /= dir.length()
-
-	# Apply gravity.
-	velocity.y += delta * gravity
-
-	# Using only the horizontal velocity, interpolate towards the input.
-	var hvel = velocity
-	hvel.y = 0
-
-	var target = dir * MAX_SPEED
-	var acceleration
-	if dir.dot(hvel) > 0:
-		acceleration = ACCELERATION
+	var lmb = Input.is_action_pressed("lmb")
+	var rmb = Input.is_action_pressed("rmb")
+	
+	camera.get_parent().translation = translation 
+	
+	if lmb and not rmb:
+		var ray_length = 100000
+		var from = camera.project_ray_origin(get_viewport().get_mouse_position())
+		var to = from + camera.project_ray_normal(get_viewport().get_mouse_position()) * ray_length
+		
+		var space_state = get_world().direct_space_state
+		if space_state.intersect_ray(from, to).values() != []:
+			target = space_state.intersect_ray(from, to).position
+	
+	if rmb and not lmb:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		camera.get_parent().rotate_y(deg2rad(my_rotation.x) * delta * mouse_sensitivity * 3)
 	else:
-		acceleration = DECELERATION
-
-	hvel = hvel.linear_interpolate(target, acceleration * delta)
-
-	# Assign hvel's values back to velocity, and then move.
-	velocity.x = hvel.x
-	velocity.z = hvel.z
+		Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED)
+	
+	velocity.y += gravity * delta
+	
+	if target:
+		look_at(target, Vector3.UP)
+		rotation.x = 0
+		velocity = -transform.basis.z * speed
+		if transform.origin.distance_to(target) < .5:
+			target = null
+			velocity = Vector3.ZERO
 	velocity = move_and_slide(velocity, Vector3.UP)
-
-	# Jumping code. is_on_floor() must come after move_and_slide().
-	if is_on_floor() and Input.is_action_pressed("jump"):
-		velocity.y = JUMP_SPEED
-
-
-func _on_tcube_body_entered(body):
-	if body == self:
-		get_node("WinText").show()
