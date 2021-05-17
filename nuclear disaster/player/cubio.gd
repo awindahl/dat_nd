8320bebeb38d2a4ddf1c6446c2855f2a885da06b
awindahl <alewin1994@gmail.com>
2021-05-18 00:04:58 +0200

    added diable style controls

--- nuclear disaster/player/cubio.gd
+++ nuclear disaster/player/cubio.gd
@@ -10,7 +10,7 @@ onready var camera = $Target/Camera
 onready var gravity = -ProjectSettings.get_setting("physics/3d/default_gravity")
 onready var start_position = translation
 var yaw = 0
-var mouse_sensitivity = 1
+var mouse_sensitivity = 2
 var my_rotation
 var target = null
 var velocity = Vector3.ZERO
@@ -39,7 +39,13 @@ func _physics_process(delta):
 		var space_state = get_world().direct_space_state
 		if space_state.intersect_ray(from, to).values() != []:
 			target = space_state.intersect_ray(from, to).position
-	
+		
+	if target:
+		if transform.origin.distance_to(target) > 5 and not lmb:
+			print(transform.origin.distance_to(target))
+			target = null
+			
+		
 	if rmb and not lmb:
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 		camera.get_parent().rotate_y(deg2rad(my_rotation.x) * delta * mouse_sensitivity * 3)
@@ -49,12 +55,22 @@ func _physics_process(delta):
 	velocity.y += gravity * delta
 	
 	if target and not busy:
-		look_at(target, Vector3.UP)
+		#look_at(target, Vector3.UP)
 		rotation.x = 0
+		rotation.z = 0
+		
+		var target_pos = target
+		var new_trans = transform.looking_at(target_pos, Vector3.UP)
+		transform = transform.interpolate_with(new_trans, delta*2)
+		
 		velocity.x = -transform.basis.z.x * speed
 		velocity.z = -transform.basis.z.z * speed
-		if transform.origin.distance_to(target) < 1 or busy:
+		if transform.origin.distance_to(target) < 2 or busy:
 			target = null
 			velocity = Vector3(0, velocity.y, 0)
+	
+	if target == null:
+		velocity = Vector3(0, velocity.y, 0)
+		
 	velocity = move_and_slide(velocity, Vector3.UP)
 	#print(velocity.y)
